0792950e9d9d30b9867f899989bee7852fac4c6d
RahulCow2021 <88501798+RahulCow2021@users.noreply.github.com>
2025-07-28 18:25:00 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_c75ebe93._.js 1753707317646

---
+++ .next/static/chunks/src_app_profile_page_tsx_c75ebe93._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f68ca3d0._.js",
+  "static/chunks/node_modules_fcd57cf5._.js"
+],
+    source: "dynamic"
+});
